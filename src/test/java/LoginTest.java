d03ff73a1fb188870cfe391f4dfe9290168f051f
StepanidaKirillina1 <steshakirillina@yandex.ru>
2025-10-29 19:21:08 +0900

    add the empty password test

---
+++ src/test/java/LoginTest.java
@@ -0,0 +1,21 @@
+import common.BaseTest;
+import org.openqa.selenium.By;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class LoginTest extends BaseTest  {
+
+    @Test
+    public void testEmptyPassword() {
+        final String standardUserLogin = "standard_user";
+        final String expectedErrorMessage = "Epic sadface: Password is required";
+
+        getDriver().findElement(By.id("user-name")).sendKeys(standardUserLogin);
+        getDriver().findElement(By.id("password")).sendKeys("");
+        getDriver().findElement(By.id("login-button")).click();
+
+        String actualErrorMessage = getDriver().findElement(By.cssSelector("h3[data-test='error']")).getText();
+
+        Assert.assertEquals(actualErrorMessage, expectedErrorMessage);
+    }
+}
